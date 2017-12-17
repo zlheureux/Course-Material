179daec1ec5d6fa59152c23a7e1dfad0c08d357e
zlheureux <zel2106@columbia.edu>
2017-12-17 12:28:55 -0700

    Assignment 6 complete.

--- MachineLearning-Coursera-Stanford-AndrewNg/Assignments/machine-learning-ex6/ex6/dataset3Params.m
+++ MachineLearning-Coursera-Stanford-AndrewNg/Assignments/machine-learning-ex6/ex6/dataset3Params.m
@@ -22,12 +22,24 @@ sigma = 0.3;
 %  Note: You can compute the prediction error using 
 %        mean(double(predictions ~= yval))
 %
+possC = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+possSig = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+errors = ones(length(possC), length(possSig));
 
 
+for i = 1:length(possC)
+	for j = 1:length(possSig)
+		model = svmTrain(X, y, possC(i), @(x1, x2) gaussianKernel(x1, x2, possSig(j))); 
+		predictions = svmPredict(model, Xval);
+		errors(i,j) = mean(double(predictions ~= yval));
+	end
+end
 
+[minval, row] = min(min(errors,[],2));
+[minval, col] = min(min(errors,[],1));
 
-
-
+C = possC(row);
+sigma = possSig(col);
 
 % =========================================================================
 
